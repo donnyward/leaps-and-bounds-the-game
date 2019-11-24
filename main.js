59f47b4c2c9b49217363584d5346baeb321fbd9f
Donny <donnyjward@gmail.com>
2019-11-23 23:09:38 -0800

    Add js
    Draws a bordered rect. Will eventually render the whole stage.

---
+++ main.js
@@ -0,0 +1,5 @@
+var screen = document.getElementById('screen');
+var context = screen.getContext('2d');
+context.fillStyle = 'red';
+context.fillRect(30, 30, 50, 50);
+context.strokeRect(30, 30, 50, 50);
