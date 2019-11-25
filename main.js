a964656f1f225bafe9a05a778ca809d633e9c5f2
Donny <donnyjward@gmail.com>
2019-11-24 16:26:07 -0800

    Draw player
    Encapsulate drawing into function

--- main.js
+++ main.js
@@ -10,17 +10,27 @@ var blocks = [[0, 0, 0, 1, 0, 0, 0, 1],
               [0, 0, 0, 1, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0],
               [1, 1, 1, 1, 0, 0, 0, 0]];
+var playerX = 0;
+var playerY = 6;
 
-context.fillStyle = 'red';
-context.strokeRect(0, 0, screen.width, screen.height);
+function draw() {
+    context.strokeRect(0, 0, screen.width, screen.height);
 
-var i;
-for (i = 0; i < blocks.length; i++) {
-    var j;
-    for (j = 0; j < blocks[i].length; j++) {
-        if (blocks[i][j] == 1) {
-            context.fillRect(boxLength * j, boxLength * i + (screen.height - (boxLength * blocks.length)), boxLength, boxLength);
-            context.strokeRect(boxLength * j, boxLength * i + (screen.height - (boxLength * blocks.length)), boxLength, boxLength);
+    var i;
+    for (i = 0; i < blocks.length; i++) {
+        var j;
+        for (j = 0; j < blocks[i].length; j++) {
+            if (blocks[i][j] == 1) {
+                context.fillStyle = 'red';
+                context.fillRect(boxLength * j, boxLength * i + (screen.height - (boxLength * blocks.length)), boxLength, boxLength);
+                context.strokeRect(boxLength * j, boxLength * i + (screen.height - (boxLength * blocks.length)), boxLength, boxLength);
+            }
         }
     }
+
+    context.fillStyle = 'green';
+    context.fillRect(boxLength * playerX, boxLength * playerY + (screen.height - (boxLength * blocks.length)), boxLength, boxLength);
+    context.strokeRect(boxLength * playerX, boxLength * playerY + (screen.height - (boxLength * blocks.length)), boxLength, boxLength);
 }
+
+draw()
